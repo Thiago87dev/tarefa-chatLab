c66be4cf1c12eda99ea7e4157910da5411987bc3
thiago alves <thiago87gsa@gmail.com>
2023-07-16 11:39:12 -0300

    first commit

--- chat.js
+++ chat.js
@@ -4,8 +4,62 @@
  * Implemente as funções a partir daqui.
  */
 
-// Exemplo: function minhaFuncao() { ... }
+const listaMensagens = []
 
+function adicionarMensagem(apelido, msg) {
+  listaMensagens.push({
+    apelido: apelido,
+    msg: msg
+  })
+}
+
+function formatarMensagens() {
+  let htmlData = ''
+  for (i in listaMensagens) {
+    let item = listaMensagens[i]
+    htmlData += `
+    <div class="chat-message">
+      <span class="chat-message-apelido">
+        ${item.apelido}
+      </span>
+      <span class="chat-message-item">
+      ${item.msg}
+      </span>
+    </div>
+`
+  }
+  return htmlData
+
+}
+
+function atualizarHTML() {
+  let resultado = document.querySelector('#chat-messages')
+  resultado.innerHTML = formatarMensagens()
+}
+
+function commitMessageClickHandler() {
+  msg = document.querySelector('#message-input')
+  if(!msg.value.trim()){
+    msg.focus()
+    return
+  }
+  adicionarMensagem('BigBoss: ', msg.value)
+  atualizarHTML()
+  msg.value = ''
+}
+
+document.addEventListener('keypress',e =>{
+  msg = document.querySelector('#message-input')
+  if (e.key === 'Enter'){
+    if(!msg.value.trim()){
+      msg.focus()
+      return
+    }
+    adicionarMensagem('BigBoss: ', msg.value)
+    atualizarHTML()
+    msg.value = ''
+  }
+})
 
 // --------------------------------
 // Não remover estas linhas
